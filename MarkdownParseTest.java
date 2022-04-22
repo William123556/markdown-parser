788020eeab25beca2911db5dd89b2a779437b4bb
Tuan2003 <37556727+Tuan2003@users.noreply.github.com>
2022-04-22 08:44:04 -0700

    Updated MarkdownParseTest.java
    Ran the following command lines in the order provided respectively.
    
    javac -cp .:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar MarkdownParseTest.java
    
    java -cp .:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar org.junit.runner.JUnitCore MarkdownParseTest

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,6 +1,16 @@
+//Importing all of the Assert methods under junit. 
 import static org.junit.Assert.*;
+//Importing necessary commands for junit, 
+//so for our output it allowed us to run the actual tests.
 import org.junit.*;
-
+//Class declaration
 public class MarkdownParseTest {
-    
+    //JUnit test annotation
+    @Test 
+    //JUnit test method declaration
+    public void addition(){
+        //Using the Assert methods 
+        //implemented for testing
+        assertEquals(2, 1+1);
+    }
 }
